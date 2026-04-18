a233c2daa0186e37f1b4449eb4f97fd83d9b7718
Tiago Sanina <tiago.sanina13@gmail.com>
2026-04-18 15:18:20 +0100

    Add patient and physiotherapist registration with Supabase insert and form validation

--- src/pages/RegistarPaciente.js
+++ src/pages/RegistarPaciente.js
@@ -1,9 +1,66 @@
-function App() {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { supabase } from '../services/supabaseClient';
+
+function RegistarPaciente() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setconfirmPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleRegisto = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError('As passwords têm que ser iguais.');
+    } else {
+      const { error: RegistrationError } = await supabase.from('users').insert({
+        email: email,
+        name: nome,
+        type: 'paciente',
+        password: password,
+      });
+
+      if (RegistrationError) {
+        setError('Erro no Registo.'); // Verificar para dar handling a outros erros
+      } else {
+        navigate('/login');
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>Registar Paciente</h1>
-    </div>
+    <form onSubmit={handleRegisto}>
+      <input
+        type="text"
+        placeholder="Nome completo"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="exemplo@email.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Introduza a sua palavra-passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Repita a palavra-passe"
+        value={confirmPassword}
+        onChange={(e) => setconfirmPassword(e.target.value)}
+      />
+      <button type="submit">Registar</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
   );
 }
 
-export default App;
+export default RegistarPaciente;
