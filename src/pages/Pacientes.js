6af54126423e10648276d1baf38f9f8f5b431fb9
Tiago Sanina <tiago.sanina13@gmail.com>
2026-04-19 12:05:48 +0100

    Pacientes screen done, list with all the "pacientes"

--- src/pages/Pacientes.js
+++ src/pages/Pacientes.js
@@ -1,9 +1,38 @@
-function App() {
+import Header from '../components/Header';
+import { supabase } from '../services/supabaseClient';
+import { useContext, useState, useEffect } from 'react';
+import { UserContext } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+
+function Pacientes() {
+  const { user } = useContext(UserContext);
+  const [listPacientes, setListPacientes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('fisioterapeuta_id', user.id);
+
+      if (!error) {
+        setListPacientes(data);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Lista Pacientes</h1>
+      <Header></Header>
+      {listPacientes.map((paciente) => (
+        <div key={paciente.id}>
+          <p>{paciente.name}</p>
+          <Link to={`/fisio/pacientes/${paciente.id}`}>Ver</Link>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default Pacientes;
