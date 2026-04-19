aa959cfeabf3d9ceb5b9026e1d74dc304542886f
Tiago Sanina <tiago.sanina13@gmail.com>
2026-04-19 13:27:05 +0100

    Add plan page with patient exercises display and Supabase joins

--- src/pages/Plano.js
+++ src/pages/Plano.js
@@ -1,9 +1,69 @@
-function App() {
+import Header from '../components/Header';
+import { supabase } from '../services/supabaseClient';
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+function Plano() {
+  const { id } = useParams();
+  const [plano, setPlano] = useState(null);
+  const [paciente, setPaciente] = useState(null);
+  const [planExercicios, setPlanoExercicios] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: dataPatient, error: errorPat } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', id)
+        .single();
+      if (!errorPat) {
+        setPaciente(dataPatient);
+      }
+      // get in plans
+      const { data, error } = await supabase
+        .from('plans')
+        .select('*')
+        .eq('patient_id', id)
+        .single();
+      if (!error) {
+        setPlano(data);
+      }
+      // get in plans exercises
+      if (data) {
+        const { data: dataPlanExercises, error: errorPlanEx } = await supabase
+          .from('plan_exercises')
+          .select(`*, exercises(*)`)
+          .eq('plan_id', data.id);
+
+        if (!errorPlanEx) {
+          setPlanoExercicios(dataPlanExercises);
+        }
+      }
+      console.log('Paciente:', dataPatient, errorPat);
+      console.log('Plano:', data, error);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Plano</h1>
+      <Header></Header>
+      {paciente && plano && (
+        <div>
+          <h1>Plano do paciente {paciente.name}</h1>
+          <p>Exercícios atribuídos ao paciente</p>
+          <Link to={`/`}>Novo Exercicio</Link>
+          <Link to={`/`}>Apagar Plano</Link>
+          {planExercicios.map((item) => (
+            <div key={item.id}>
+              <p>{item.exercises.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default Plano;
